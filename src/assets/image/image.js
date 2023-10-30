8c7a7d835d05d74c152f29bbc1c25de5085dae80
agungnrprstya <agungnurprasetya@gmail.com>
2023-10-30 13:16:18 +0700

    update logo and image

---
+++ src/assets/image/image.js
@@ -0,0 +1,13 @@
+import carouselImage1 from "./carousel-1.jpg";
+import carouselImage2 from "./carousel-2.jpg";
+import carouselImage3 from "./carousel-3.jpg";
+import carouselImage4 from "./carousel-4.jpg";
+import carouselImage5 from "./carousel-5.jpg";
+
+export const carouselImages = [carouselImage1, carouselImage2, carouselImage3, carouselImage4, carouselImage5];
+
+import interior1 from "./interior-1.jpg";
+import interior2 from "./interior-2.jpg";
+import interior3 from "./interior-3.jpg";
+
+export const interiorImages = [interior1, interior2, interior3];
